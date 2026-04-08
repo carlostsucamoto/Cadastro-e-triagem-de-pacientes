45a154f1e35a9e00f8fb6af9c00ebe5fb5b9cd71
carlostsucamoto <caduchiarelli@hotmail.com>
2026-04-07 23:09:24 -0300

    feat: finalizado o teste mocado

--- src/br/com/fiap/main/Main.java
+++ src/br/com/fiap/main/Main.java
@@ -1,4 +1,17 @@
 package br.com.fiap.main;
 
+import br.com.fiap.bean.Paciente;
+
 public class Main {
-}
+        public static void main(String[] args) {
+            //teste mockado
+            Paciente paciente1 = new Paciente();
+            paciente1.setNome("Carlos Eduardo");
+            paciente1.setAnoNascimento(2000);
+            paciente1.setNivelDor(8);
+            paciente1.setSintomas("tosse");
+            System.out.printf("ola! %s verifiquei que o senhor possui %d anos,notei também que você esta sentindo um nível %d de dor e que seu sintoma é %s", paciente1.getNome(),paciente1.calcularIdade(),paciente1.getNivelDor(),paciente1.getSintomas());
+            System.out.printf("\nE sua prioridade é: %s",paciente1.definirPrioridade());
+        }
+    }
+
